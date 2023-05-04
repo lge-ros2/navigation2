52da638834e7e3d2e903c26537cf6790483b71e8
sungkyu.kang <sungkyu.kang@lge.com>
2023-05-04 11:37:40 +0900

    modify subscribe planner with prune path

--- nav2_subscribe_planner/src/nav2_subscribe_planner.cpp
+++ nav2_subscribe_planner/src/nav2_subscribe_planner.cpp
@@ -35,6 +35,7 @@
 #include "builtin_interfaces/msg/duration.hpp"
 #include "nav2_util/costmap.hpp"
 #include "nav2_util/node_utils.hpp"
+#include "nav2_util/geometry_utils.hpp"
 #include "nav2_costmap_2d/cost_values.hpp"
 
 using namespace std::chrono_literals;
@@ -77,6 +78,9 @@ SubscribePlanner::configure(
   declare_parameter_if_not_declared(node, name + ".topic", rclcpp::ParameterValue("plan"));
   node->get_parameter(name + ".topic", topic_name_);
 
+  declare_parameter_if_not_declared(node, name + ".path_pruning", rclcpp::ParameterValue(true));
+  node->get_parameter(name + ".path_pruning", path_pruning_);
+
   auto callback = [this](const nav_msgs::msg::Path::SharedPtr msg) -> void {
     path_ = *msg;
   };
@@ -113,11 +117,71 @@ SubscribePlanner::cleanup()
 }
 
 nav_msgs::msg::Path SubscribePlanner::createPlan(
-  const geometry_msgs::msg::PoseStamped & /*start*/,
-  const geometry_msgs::msg::PoseStamped & /*goal*/)
+  const geometry_msgs::msg::PoseStamped & start,
+  const geometry_msgs::msg::PoseStamped & /* goal */)
 {
-  return path_;
+
+  if (path_.poses.empty()) {
+    return path_;
+  }
+
+  double angular_distance_weight = 0.0;
+  double max_robot_pose_search_dist = std::numeric_limits<double>::infinity();
+  double distance_forward = std::numeric_limits<double>::infinity();
+
+
+  closest_pose_detection_begin_ = path_.poses.begin();
+  auto closest_pose_detection_end = path_.poses.end();
+
+  if (path_pruning_) {
+    closest_pose_detection_end = nav2_util::geometry_utils::first_after_integrated_distance(
+      closest_pose_detection_begin_, path_.poses.end(), max_robot_pose_search_dist);
+  }
+
+
+  // find the closest pose on the path
+  auto current_pose = nav2_util::geometry_utils::min_by(
+    closest_pose_detection_begin_, closest_pose_detection_end,
+    [&start, angular_distance_weight](const geometry_msgs::msg::PoseStamped & ps) {
+      return poseDistance(start, ps, angular_distance_weight);
+    });
+
+  if (path_pruning_) {
+    closest_pose_detection_begin_ = current_pose;
+  }
+
+  // expand forwards to extract desired length
+  auto forward_pose_it = nav2_util::geometry_utils::first_after_integrated_distance(
+    current_pose, path_.poses.end(), distance_forward);
+  // expand backwards to extract desired length
+  // Note: current_pose + 1 is used because reverse iterator points to a cell before it
+  auto backward_pose_it = nav2_util::geometry_utils::first_after_integrated_distance(
+    std::reverse_iterator(current_pose + 1), path_.poses.rend(), 0.0);
+  nav_msgs::msg::Path output_path;
+  output_path.header = path_.header;
+  output_path.poses = std::vector<geometry_msgs::msg::PoseStamped>(
+    backward_pose_it.base(), forward_pose_it);
+  return output_path;
 }
+
+double
+SubscribePlanner::poseDistance(
+  const geometry_msgs::msg::PoseStamped & pose1,
+  const geometry_msgs::msg::PoseStamped & pose2,
+  const double angular_distance_weight)
+{
+  double dx = pose1.pose.position.x - pose2.pose.position.x;
+  double dy = pose1.pose.position.y - pose2.pose.position.y;
+  // taking angular distance into account in addition to spatial distance
+  // (to improve picking a correct pose near cusps and loops)
+  tf2::Quaternion q1;
+  tf2::convert(pose1.pose.orientation, q1);
+  tf2::Quaternion q2;
+  tf2::convert(pose2.pose.orientation, q2);
+  double da = angular_distance_weight * std::abs(q1.angleShortestPath(q2));
+  return std::sqrt(dx * dx + dy * dy + da * da);
+}
+
 
 }  // namespace nav2_subscribe_planner
 
