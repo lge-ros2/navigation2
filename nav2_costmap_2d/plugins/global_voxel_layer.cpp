cfcc102de85c68c06cb411a5141dfdb9516b981b
sungkyu.kang <sungkyu.kang@lge.com>
2023-06-22 16:54:43 +0900

    add raytrace unkowon space in global_voxel_layer

--- nav2_costmap_2d/plugins/global_voxel_layer.cpp
+++ nav2_costmap_2d/plugins/global_voxel_layer.cpp
@@ -70,21 +70,21 @@ void GlobalVoxelLayer::onInitialize()
     bool inf_is_valid;
     double publish_cycle;
     std::string cmd_vel_topic;
+    std::string robot_frame_id;
     double rotate_threshold;
 
     declareParameter(source + "." + "init_scan_angle", rclcpp::ParameterValue(true));
-    declareParameter(source + "." + "scan_link_offset", rclcpp::ParameterValue(2.0));
     declareParameter(source + "." + "publish_cycle", rclcpp::ParameterValue(1.0));
     declareParameter(source + "." + "cmd_vel_topic", rclcpp::ParameterValue("/cmd_vel"));
+    declareParameter(source + "." + "robot_frame_id", rclcpp::ParameterValue("base_link"));
     declareParameter(source + "." + "rotate_threshold", rclcpp::ParameterValue(0.17));
 
     node->get_parameter(name_ + "." + source + "." + "data_type", data_type);
     node->get_parameter(name_ + "." + source + "." + "inf_is_valid", inf_is_valid);
     node->get_parameter(name_ + "." + source + "." + "init_scan_angle", init_scan_angle);
-    node->get_parameter(name_ + "." + source + "." + "scan_link_offset", scan_link_offset_);
     node->get_parameter(name_ + "." + source + "." + "publish_cycle", publish_cycle);
-    node->get_parameter(name_ + "." + source + "." + "scan_link_offset", scan_link_offset_);
     node->get_parameter(name_ + "." + source + "." + "cmd_vel_topic", cmd_vel_topic);
+    node->get_parameter(name_ + "." + source + "." + "robot_frame_id", robot_frame_id);
     node->get_parameter(name_ + "." + source + "." + "rotate_threshold", rotate_threshold);
 
     if (publish_cycle > 0) {
@@ -114,6 +114,7 @@ void GlobalVoxelLayer::onInitialize()
     use_init_scan_angle_ = init_scan_angle;
     cmd_vel_topic_string = cmd_vel_topic;
     rotate_threshold_ = rotate_threshold;
+    robot_frame_id_ = robot_frame_id;
   }
 
   // [sungkyu.kang] create for obstacle index
@@ -146,6 +147,7 @@ void GlobalVoxelLayer::onInitialize()
       cost_translation_table_[i] = FREE_SPACE;
     }
   }
+  scan_link_offset_ = -999.0;
 }
 
 void
@@ -185,8 +187,11 @@ GlobalVoxelLayer::initializeScanAngle(sensor_msgs::msg::LaserScan::ConstSharedPt
     scan_end_angle_ = message->angle_max;
   }
 
-  RCLCPP_INFO(logger_, "    scan_start_angle: %f, scan_end_angle_: %f", scan_start_angle_, scan_end_angle_);
+  scan_frame_id_ = message->header.frame_id;
+  RCLCPP_INFO(logger_, "    scan_start_angle: %.2f, scan_end_angle_: %.2f, scan_frame_id: %s", scan_start_angle_, scan_end_angle_, scan_frame_id_.c_str());
   is_init_scan_angle_ = true;
+
+
 }
 
 void
@@ -197,6 +202,7 @@ GlobalVoxelLayer::laserScanCallback(
   if (!is_init_scan_angle_) {
     initializeScanAngle(message);
   }
+  last_scan_msg_ = message;
   ObstacleLayer::laserScanCallback(message, buffer);
 }
 
@@ -208,14 +214,17 @@ GlobalVoxelLayer::laserScanValidInfCallback(
   if (!is_init_scan_angle_) {
     initializeScanAngle(raw_message);
   }
+  last_scan_msg_ = raw_message;
   ObstacleLayer::laserScanValidInfCallback(raw_message, buffer);
 }
 
-void GlobalVoxelLayer::updateRobotYaw(const double robot_yaw)
+void GlobalVoxelLayer::updateRobotPose(const double robot_x, const double robot_y, const double robot_yaw)
 {
-  // [sungkyu.kang] set current_robot_yaw_
+  // [sungkyu.kang] set current_robot_pose
+  current_robot_x_ = robot_x;
+  current_robot_y_ = robot_y;
   current_robot_yaw_ = robot_yaw;
-  // RCLCPP_INFO(logger_, "updateBounds - robot_yaw: %f, current_robot_yaw_: %f", robot_yaw, current_robot_yaw_);
+  // RCLCPP_INFO(logger_, "updateBounds - robot_pose: (%.2f, %.2f| %.2f)", current_robot_x_, current_robot_y_, current_robot_yaw_);
 }
 
 void GlobalVoxelLayer::updateBounds(
@@ -225,7 +234,7 @@ void GlobalVoxelLayer::updateBounds(
 {
   {
     std::lock_guard<Costmap2D::mutex_t> guard(*getMutex());
-    updateRobotYaw(robot_yaw);
+    updateRobotPose(robot_x, robot_y, robot_yaw);
   }
 
   VoxelLayer::updateBounds(robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y);
@@ -259,6 +268,7 @@ void GlobalVoxelLayer::updateCosts(
 
 void GlobalVoxelLayer::prepareGrid(nav2_costmap_2d::Costmap2D costmap)
 {
+  // RCLCPP_INFO(logger_, "GlobalVoxelLayer::prepareGrid start");
   std::unique_lock<Costmap2D::mutex_t> lock(*(costmap.getMutex()));
   float grid_resolution = costmap.getResolution();
   unsigned int grid_width = costmap.getSizeInCellsX();
@@ -280,40 +290,229 @@ void GlobalVoxelLayer::prepareGrid(nav2_costmap_2d::Costmap2D costmap)
   grid_->info.origin.position.y = wy - grid_resolution / 2;
   grid_->info.origin.position.z = 0.0;
   grid_->info.origin.orientation.w = 1.0;
-
   grid_->data.resize(grid_->info.width * grid_->info.height);
+  std::fill(grid_->data.begin(), grid_->data.end(), -1);
 
+  // costmap original data
   unsigned char * data = costmap.getCharMap();
 
-  double normalLeftX = -1 * sin(current_robot_yaw_ - scan_start_angle_);
-  double normalLeftY = cos(current_robot_yaw_ - scan_start_angle_);
-  double normalRightX = sin(current_robot_yaw_ - scan_end_angle_);
-  double normalRightY = -1 * cos(current_robot_yaw_ - scan_end_angle_);
+  // for (unsigned int i = 0; i < grid_->data.size(); i++) {
+  //   grid_->data[i] = cost_translation_table_[data[i]];
+  //   if (data[i] == 100) {
+  //     RCLCPP_INFO(logger_, "          obstacle - idx: %d", i);
+  //   }
+  // }
 
-  // RCLCPP_INFO(
-  //   logger_,
-  //   "\nGlobalVoxelLayer::prepareGrid %f, %f, %f.\n    %f, %f, %f, %f", current_robot_yaw_, current_robot_yaw_ - scan_start_angle_, current_robot_yaw_ - scan_end_angle_, normalLeftX, normalLeftY, normalRightX, normalRightY);
-  int x, y;
-  unsigned int row_index, col_index;
-  int scan_tf_offset_x = round(scan_link_offset_ * cos(current_robot_yaw_));
-  int scan_tf_offset_y = round(scan_link_offset_ * sin(current_robot_yaw_));
-  // RCLCPP_INFO(logger_, "scan_tf_offset_x: %d, scan_tf_offset_y: %d", scan_tf_offset_x, scan_tf_offset_y);
-  for (unsigned int i = 0; i < grid_->data.size(); i++) {
-    // [sungkyu.kang] check robot orientation. fill -1 back of robot.
-    row_index = i / grid_width;
-    col_index = i % grid_width;
-    x = col_index - grid_height / 2 - scan_tf_offset_x;
-    y = row_index - grid_width / 2 - scan_tf_offset_y;
-    // RCLCPP_INFO(
-    //   logger_,
-    //   " [%4d] row_index: %2d, col_index: %2d, (%2d, %2d)", i, row_index, col_index, x, y);
-    if (x * normalLeftX + y * normalLeftY > 0 && x * normalRightX + y * normalRightY > 0) {
-      // RCLCPP_INFO( logger_,"    hit!!");
-      grid_->data[i] = -1;
-    } else {
-      grid_->data[i] = cost_translation_table_[data[i]];
+  // [sungkyu.kang] check robot orientation. fill -1 back of robot.
+  if (scan_link_offset_ == -999.0) {
+    // initialize scan-link_offset
+    auto transform_stamped = tf_->lookupTransform(
+      robot_frame_id_, scan_frame_id_, tf2::TimePointZero,
+      tf2::durationFromSec(0.5));
+
+    scan_link_offset_ = transform_stamped.transform.translation.x;
+    RCLCPP_INFO(logger_, "scan_link_offset: %.2f", scan_link_offset_);
+
+  }
+
+  // RCLCPP_INFO(logger_, "  wx: %.2f, wy: %.2f, grid_width: %d, grid_height: %d, grid_resolution: %.2f", wx, wy, grid_width, grid_height, grid_resolution);
+  // RCLCPP_INFO(logger_, "  current_robot_x_: %.2f, current_robot_y_: %.2f, current_robot_yaw_: %.2f", current_robot_x_, current_robot_y_, current_robot_yaw_);
+
+  double scan_wx = current_robot_x_ + scan_link_offset_ * cos(current_robot_yaw_);
+  double scan_wy = current_robot_y_ + scan_link_offset_ * sin(current_robot_yaw_);
+
+  int scan_mx, scan_my;
+  costmap.worldToMapNoBounds(scan_wx, scan_wy, scan_mx, scan_my);
+
+  double scan_start_angle = current_robot_yaw_ - scan_start_angle_;
+  double scan_end_angle = current_robot_yaw_ - scan_end_angle_;
+
+  double scan_check_radius = (double)grid_width / 2  * sqrt(2) * grid_resolution;
+  int bf_mx, bf_my;
+
+  // RCLCPP_INFO(logger_, "  scan_wx: %.2f, scan_wy: %.2f, scan_mx: %d, scan_my: %d, radius: %.2f, yaw: %.2f", scan_wx, scan_wy, scan_mx, scan_my, scan_check_radius, current_robot_yaw_);
+
+  // get scan_start endpoint
+  double bf_wx = scan_wx + scan_check_radius * std::cos(scan_start_angle);
+  double bf_wy = scan_wy + scan_check_radius * std::sin(scan_start_angle);
+
+  costmap.worldToMapNoBounds(bf_wx, bf_wy, bf_mx, bf_my);
+
+  auto b_result = bresenham(scan_mx, scan_my, bf_mx, bf_my, grid_width, grid_height);
+  int start_mx = b_result[b_result.size()-1][0];
+  int start_my = b_result[b_result.size()-1][1];
+  // RCLCPP_INFO(logger_, "    scan_start_angle: %.2f, start_mx: %d, start_my: %d", scan_start_angle, start_mx, start_my);
+
+  // get scan_end endpoint
+  bf_wx = scan_wx + scan_check_radius * std::cos(scan_end_angle);
+  bf_wy = scan_wy + scan_check_radius * std::sin(scan_end_angle);
+
+  costmap.worldToMapNoBounds(bf_wx, bf_wy, bf_mx, bf_my);
+
+  b_result = bresenham(scan_mx, scan_my, bf_mx, bf_my, grid_width, grid_height);
+  int end_mx = b_result[b_result.size()-1][0];
+  int end_my = b_result[b_result.size()-1][1];
+  // RCLCPP_INFO(logger_, "    scan_end_angle: %.2f, end_mx: %d, end_my: %d", scan_end_angle, end_mx, end_my);
+
+  auto direction = SPIRAL_DIRECTION::RIGHT;
+  if (start_mx == 0) {
+    direction = SPIRAL_DIRECTION::UP;
+  } else if (start_my == 0) {
+    direction = SPIRAL_DIRECTION::LEFT;
+  } else if (start_mx >= (int)grid_width -1) {
+    direction = SPIRAL_DIRECTION::DOWN;
+  }
+
+  int curr_mx = start_mx;
+  int curr_my = start_my;
+  // RCLCPP_INFO(logger_, "      curr_mx: %d, curr_my: %d", curr_mx, curr_my);
+  while(curr_mx != end_mx || curr_my != end_my) {
+    // RCLCPP_INFO(logger_, "      curr_mx: %d, curr_my: %d", curr_mx, curr_my);
+    fillGrid(scan_mx, scan_my, curr_mx, curr_my, grid_width, grid_height, data);
+
+    switch (direction)
+    {
+      case SPIRAL_DIRECTION::RIGHT:
+        if (curr_mx == (int)grid_width - 1) {
+          // RCLCPP_INFO(logger_, "          change direction R -> D");
+          direction = SPIRAL_DIRECTION::DOWN;
+          curr_my -= 1;
+        } else {
+          curr_mx += 1;
+        }
+        break;
+      case SPIRAL_DIRECTION::DOWN:
+        if (curr_my == 0) {
+          // RCLCPP_INFO(logger_, "          change direction D -> L");
+          direction = SPIRAL_DIRECTION::LEFT;
+          curr_mx -= 1;
+        } else {
+          curr_my -= 1;
+        }
+        break;
+      case SPIRAL_DIRECTION::LEFT:
+        if (curr_mx == 0) {
+          // RCLCPP_INFO(logger_, "          change direction L -> U");
+          direction = SPIRAL_DIRECTION::UP;
+          curr_my += 1;
+        } else {
+          curr_mx -= 1;
+        }
+        break;
+      case SPIRAL_DIRECTION::UP:
+        if (curr_my == (int)grid_height - 1) {
+          // RCLCPP_INFO(logger_, "          change direction U -> R");
+          direction = SPIRAL_DIRECTION::RIGHT;
+          curr_mx += 1;
+        } else {
+          curr_my += 1;
+        }
+        break;
     }
   }
-  // RCLCPP_INFO(logger_, "GlobalVoxelLayer::prepareGrid finish ");
+  fillGrid(scan_mx, scan_my, end_mx, end_my, grid_width, grid_height, data);
+
+  // RCLCPP_INFO(logger_, "GlobalVoxelLayer::prepareGrid end");
 }
+
+void GlobalVoxelLayer::fillGrid(int s_x, int s_y, int f_x, int f_y, int width, int height, unsigned char * data)
+{
+  int OBSTACLE_DATA_VALUE = 254;
+  auto b_result = bresenham(s_x, s_y, f_x, f_y, width, height);
+  std::vector<std::array<int, 2>>::iterator iter;
+  bool is_obstacle = false;
+  for (iter = b_result.begin(); iter != b_result.end(); iter++) {
+    int x = (*iter)[0];
+    int y = (*iter)[1];
+    int idx = y * width + x;
+    // RCLCPP_INFO(logger_, "        x: %d, y: %d, idx: %d, data: %d", x, y, idx, data[idx]);
+    if (data[idx] == OBSTACLE_DATA_VALUE) {
+      is_obstacle = true;
+      grid_->data[idx] = 100;
+      // RCLCPP_INFO(logger_, "          obstacle!!!!");
+    } else if (is_obstacle) {
+      grid_->data[idx] = -1;
+    } else {
+      grid_->data[idx] = 0;
+    }
+  }
+}
+
+std::vector<std::array<int, 2>> GlobalVoxelLayer::bresenham(
+  int s_x, int s_y,
+  int f_x, int f_y,
+  int width, int height)
+{
+  // RCLCPP_INFO(logger_, "        bresenham s_x: %d, s_y: %d. f_x: %d, f_y: %d", s_x, s_y, f_x, f_y);
+  std::vector<std::array<int, 2>> result;
+
+  int x = s_x;
+  int y = s_y;
+  int W = f_x - s_x;
+  int H = f_y - s_y;
+  int inc_x = 1;
+  if (W < 0) {
+    inc_x = -1;
+  } else if (W == 0) {
+    inc_x = 0;
+  }
+
+  int inc_y = 1;
+  if (H < 0) {
+    inc_y = -1;
+  } else if (H == 0) {
+    inc_y = 0;
+  }
+
+  W = std::abs(W);
+  H = std::abs(H);
+
+  // 0 < 기울기 <= 1
+  if ( W >= H ) {
+    // 초기값
+    int F = 2 * H - W; // F: 27
+
+    // 각 판별식 공식
+    int dF_1 = 2 * H;
+    int dF_2 = 2 * (H - W);
+
+    for (; x < width && y < height && x >= 0 && y >= 0; x += inc_x)
+    {
+      result.push_back({x, y});
+      // 중단점이 0보다 작으면 그에 맞는 판별식으로 갱신, y 값은 그대로
+      if (F < 0)
+        F += dF_1;
+      // 중단점이 0보다 크거나 같으면 그에 맞는 판별식으로 갱신, y 값 증가
+      else
+      {
+        y += inc_y;
+        F += dF_2;
+      }
+
+    }
+  } else {
+    // 초기값
+    int F = 2 * W - H;
+
+    // 각 판별식 공식
+    int dF_1 = 2 * W;
+    int dF_2 = 2 * (W - H);
+
+    for (; x < width && y < height && x >= 0 && y >= 0; y += inc_y)
+    {
+      result.push_back({x, y});
+      // 중단점이 0보다 작으면 그에 맞는 판별식으로 갱신, x 값은 그대로
+      if (F < 0)
+        F += dF_1;
+      // 중단점이 0보다 크거나 같으면 그에 맞는 판별식으로 갱신, x 값 증가
+      else
+      {
+        x += inc_x;
+        F += dF_2;
+      }
+    }
+  }
+  return result;
+}
+
 }  // namespace nav2_costmap_2d
